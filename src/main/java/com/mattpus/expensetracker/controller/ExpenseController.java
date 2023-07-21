3d2a1e36ba31c4d7a744c9a2ea9cdde0e066b9b9
mateusz.pusiewicz <mateusz.pusiewicz@ing.com>
2023-07-21 15:20:51 +0200

    Add filtering by date

--- src/main/java/com/mattpus/expensetracker/controller/ExpenseController.java
+++ src/main/java/com/mattpus/expensetracker/controller/ExpenseController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -57,4 +58,10 @@ public class ExpenseController {
         return expenseService.readByName(keyword, page);
     }
 
+    @GetMapping("expenses/date")
+    public List<Expense> getExpenseByDateBetween(@RequestParam(required = false) Date startDate,
+                                                 @RequestParam(required = false) Date endDate,
+                                                 Pageable page) {
+        return expenseService.readByDate(startDate, endDate, page);
+    }
 }
--- src/main/java/com/mattpus/expensetracker/repository/ExpenseRepository.java
+++ src/main/java/com/mattpus/expensetracker/repository/ExpenseRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 
     Page<Expense> findByCategory(String category, Pageable page);
     Page<Expense> findByNameContaining(String keyword, Pageable page);
+    Page<Expense> findByDateBetween(Date startDate, Date endDate, Pageable page);
 }
